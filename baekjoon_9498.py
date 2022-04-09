2de81222c8f28c1510633c67716f054415c74df7
cutekamji <102572991+cutekamji@users.noreply.github.com>
2022-04-09 21:07:22 +0900

    Create baekjoon_9498.py

---
+++ baekjoon_9498.py
@@ -0,0 +1,9 @@
+# 시험 성적
+# https://www.acmicpc.net/problem/9498
+
+scr = int(input())
+if scr>=90 and scr<=100: print("A")
+elif scr>=80: print("B")
+elif scr>=70: print("C")
+elif scr>=60: print("D")
+else: print("F")
