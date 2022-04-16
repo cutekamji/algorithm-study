d01bdd0e6334d2c7fc0317f7a8c17cf1737a66e7
cutekamji <102572991+cutekamji@users.noreply.github.com>
2022-04-16 20:30:53 +0900

    Create baekjoon_11021.py

---
+++ baekjoon_11021.py
@@ -0,0 +1,10 @@
+# A+B - 7
+# https://www.acmicpc.net/problem/11021
+
+import sys
+
+t = int(input())
+
+for i in range(t):
+    a, b = map(int, sys.stdin.readline().split())
+    print(f'Case #{i+1}: {a+b}')
