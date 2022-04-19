9772f7c29edbb830c62e1bffa70c100bc4af2aeb
cutekamji <102572991+cutekamji@users.noreply.github.com>
2022-04-19 16:55:00 +0900

    Create baekjoon_10951.py
    try-except

---
+++ baekjoon_10951.py
@@ -0,0 +1,10 @@
+# A+B - 4
+# https://www.acmicpc.net/problem/10951
+
+import sys
+while True:
+    try:
+        a,b = map(int, sys.stdin.readline().split())
+        print(a+b)
+    except:
+        break
